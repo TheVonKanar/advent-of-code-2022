897d169e18bc574b6fd8073e87b730eddbeb64e4
Thibault Eynard <thibault.eynard@hotmail.fr>
2023-01-25 20:04:51 +1300

    Day 1

--- src/solvers/solver_01.rs
+++ src/solvers/solver_01.rs
@@ -1,9 +1,22 @@
 pub(super) fn resolve(puzzle: String) -> (usize, usize) {
-    eprintln!("{}", puzzle);
-    (1, 1)
+    let mut loads: Vec<usize> = vec![];
+    let mut current: usize = 0;
+    for line in puzzle.lines() {
+        match line.parse::<usize>() {
+            Ok(calories) => current += calories,
+            Err(_) => {
+                loads.push(current);
+                current = 0;
+            }
+        }
+    }
+
+    loads.sort_by(|a, b| b.cmp(a));
+    
+    (loads[0], loads[0] + loads[1] + loads[2])
 }
 
 #[test]
 fn test_01() {
-    assert_eq!(resolve(crate::solvers::get_puzzle(1)), (1, 1));
+    assert_eq!(resolve(crate::solvers::get_puzzle(1)), (69281, 201524));
 }
